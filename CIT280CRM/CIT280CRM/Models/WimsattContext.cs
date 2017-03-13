406d07e3c29d961c79f11faa6b36ec8c0226ad5f
jacobs33 <jacobs33@JB202-LB22-W-D>
2017-03-13 09:20:27 -0400

    DB

---
+++ CIT280CRM/CIT280CRM/Models/WimsattContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace CIT280CRM.Models
+{
+    public class CRMDb : DbContext
+    {
+        public CRMDb() : base("CRMDb")
+        {
+
+        }
+
+        
+    }
+}
